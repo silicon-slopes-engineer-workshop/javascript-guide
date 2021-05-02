dea92aa0b1b93f91beec0bfd5f963f94d77c1b51
Chris Vo <chris@yikesinc.com>
2021-05-02 10:01:53 -0400

    05022021-update installation pages

---
+++ 00-installation/visual/incl.js
@@ -0,0 +1,31 @@
+fetch("head.html")
+	.then(response => {
+		return response.text()
+	})
+	.then(data => {
+		document.querySelector("head").innerHTML = data;
+	});
+
+fetch("../../src/include/header.html")
+	.then(response => {
+		return response.text()
+	})
+	.then(data => {
+		document.querySelector("header").innerHTML = data;
+	});
+
+fetch("../../src/include/nav.html")
+	.then(response => {
+		return response.text()
+	})
+	.then(data => {
+		document.querySelector("nav").innerHTML = data;
+	});
+
+fetch("../../src/include/footer.html")
+	.then(response => {
+		return response.text()
+	})
+	.then(data => {
+		document.querySelector("footer").innerHTML = data;
+	});
