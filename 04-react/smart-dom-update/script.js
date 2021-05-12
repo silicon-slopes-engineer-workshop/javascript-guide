70486cb13ab89cc6641b51ae5c5906596cb63bf4
Chris Vo <vochris3@gmail.com>
2021-05-11 21:53:55 -0400

    05112021-change names

--- 04-react/smart-dom-update/script.js
+++ 04-react/smart-dom-update/script.js
@@ -2,13 +2,13 @@ const jsDiv = document.getElementById("dateJs");
 const reactDiv = document.getElementById("dateReact");
 
 const render = () => {
-    jsDiv.innerHTML = `
-        <div class="split">
-            JS template
-            <input />
-            <p>${new Date()}</p>
-        </div>
-    `;
+	jsDiv.innerHTML = `
+		<div class="split">
+			JS template
+			<input />
+			<p>${new Date()}</p>
+		</div>
+	`;
 }
 
 setInterval(render, 100);
